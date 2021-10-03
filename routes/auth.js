bd4603f47fdcf6618df6019b676b041f5c79963f
sahandp2012p <sahandclevr@gmail.com>
2021-10-03 05:39:15 +0330

    Added user model in file

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.post('/register', (req, res) => {
-    res.send('Register')
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
 })
 
 module.exports = router
