88210ff2a292a56e3dc7a45dd43652445588bb6c
sahandp2012p <sahandclevr@gmail.com>
2021-10-03 05:35:07 +0330

    Created user schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 10,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
