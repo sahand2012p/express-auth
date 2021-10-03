7ecdcd24cc07cca11bdd5d998e2ec1c180f44f27
sahandp2012p <sahandclevr@gmail.com>
2021-10-03 05:52:21 +0330

    Added validation schema with Joi

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,14 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
+const Joi = require('@hapi/joi')
+
+const valSchema = {
+    name: Joi.string().min(1).max(255).required(),
+    email: Joi.string().min(10).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+}
+
 router.post('/register', async (req, res) => {
     const user = new User({
         name: req.body.name,
